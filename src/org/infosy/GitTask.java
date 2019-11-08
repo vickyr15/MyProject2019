2e40a7a323bb227a289c5e2f53f638d926a49d64
vickyr15 <vigneshragunathr@gmail.com>
2019-11-08 17:17:07 +0530

    I have done my project

--- src/org/infosy/GitTask.java
+++ src/org/infosy/GitTask.java
@@ -1,10 +1,13 @@
 package org.infosy;
 
 public class GitTask {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public void age()
+	{
+System.out.println("26");
+}
+	public static void main(String[] args) 
+	{
+		
 
 	}
-
 }
